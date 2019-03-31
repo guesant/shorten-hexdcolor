18e578132a090325eae9453a3b36aca3156943e2
Gabriel Rodrigues <jipacoding@gmail.com>
2019-03-31 15:28:33 -0400

    feat: create algorithmn

---
+++ src/tools/index.js
@@ -0,0 +1,9 @@
+import standardize from './standardize';
+import validate from './validate';
+import others from './others';
+
+export default {
+  standardize,
+  validate,
+  others,
+}
---
+++ src/tools/standardize/index.js
@@ -0,0 +1,9 @@
+import splitColor from './split-color';
+import standardizeColor from './standardize-color';
+import evenLength from './even-length';
+
+export default {
+  evenLength,
+  splitColor,
+  standardizeColor,
+}
