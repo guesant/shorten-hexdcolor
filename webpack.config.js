b9de9ccc5459471e12a2955fc6bce52941b639dd
Gabriel Rodrigues <jipacoding@gmail.com>
2019-10-11 15:09:37 -0400

    feat: generate bundle with webpack and babel
    Os comandos `dev:build` e `dev:watch` agora
    utilizam o webpack como compilador e/ou transpilador.
    
    O gulp foi removido, visto que seu propósito era unicamente
    execudar os comandos acima.
    
    closes issue #7

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'lib'),
+    filename: 'index.js',
+    libraryExport: 'default',
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
